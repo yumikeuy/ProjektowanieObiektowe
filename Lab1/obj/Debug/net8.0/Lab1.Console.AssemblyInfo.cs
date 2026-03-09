4b6c578dfc640f00687edd123de6f346e5ad5300
yumikeuy <mishanchik.yushkevich@gmail.com>
2026-03-09 16:03:09 +0100

    added sln

--- Lab1/obj/Debug/net8.0/Lab1.Console.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f8fb61cc3a998a2a7daca32302b340dbdd1aa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
